39db2c503ae111ed5c78b622c736bc67c6282b34
milly <138996581+m1llyy@users.noreply.github.com>
2024-02-25 18:44:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,6 @@ from recommender import recommend_similar_songs  # Import your recommendation fu
 
 app = Flask(__name__)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def homepage():
-#     if request.method == 'POST':
-#         user_input = request.form['user_input']
-#         recommended_songs = recommend_similar_songs(user_input)
-#         return render_template('homepage.html', user_input=user_input, recommended_songs=recommended_songs)
-#     return render_template('homepage.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 @app.route('/')
 def index():
     return render_template('homepage.html', recommended_songs=[])
